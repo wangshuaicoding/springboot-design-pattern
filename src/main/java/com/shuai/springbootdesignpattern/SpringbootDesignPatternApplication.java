a55a0372af61a986c8760bb6bb67ae1b306089ed
王帅 <2895788257@qq.com>
2025-03-14 17:54:56 +0800

    学习设计模式项目初建

---
+++ src/main/java/com/shuai/springbootdesignpattern/SpringbootDesignPatternApplication.java
@@ -0,0 +1,14 @@
+package com.shuai.springbootdesignpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class SpringbootDesignPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDesignPatternApplication.class, args);
+	}
+
+}
